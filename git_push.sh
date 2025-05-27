2a00c51811ccd31e7200524d888302f193232ef5
TupitsyiV <tupitskyi.v@donnu.edu.ua>
2025-05-27 22:09:41 +0000

    Initial commit

--- git_push.sh
+++ git_push.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+
 if ["$#" -ne 2 ]; then
    echo "Usage: $0 /local/folder/path git@github.com:TupitskyiV/repo.git"
    exit 1
 fi
 
+
 LOCAL_DIR=$1
 REMOTE_REPO=$2
 
+
 if [ ! -d "$LOCAL_DIR" ]; then
-   echo ""
+   echo "Local directory does not exist."
    exit 1
 fi
 
+
 cd  "$LOCAL_DIR" || exit 1
+
 
 if [ ! -d ".git" ]; then
   git init
 fi
+
+
 git add .
 git commit -m "Initial commit"
 
-git remote add origin "$REMOTE_REPO" 2>/dev/nuli || git remote set-url origin "REMOTE_REPO"
-git branch -M main 2>/dev/null
+
+git remote add origin "$REMOTE_REPO" 2>/dev/nuli || git remote set-url origin "$REMOTE_REPO"
+git branch -M main 
+
 
 git push -u origin main
