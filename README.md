112d60f7bfd39a7b92dca1f69298aff23dd79504
TupitsyiV <tupitskyi.v@donnu.edu.ua>
2025-05-28 05:22:03 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Lab 2 OSSP
+
+ # Lab Work 2 - Operating Systems and Systems Programming
+
+
+ This project contains automation scripts for:
+ - Creating Linux users with SSH keys
+ - Initializing and pushing local directories to a remote Git repository
+
+
+ ## Scripts
+
+ ### create_users.sh
+ - Logs actions to `create_users.log`
+ - `create_users.sh` - creat users and generates passwords + SSH keys
+ - Save paswords in `*_password.txt`
+ - Appends CSV lines to `users.csv`
+
+ ### git_push.sh
+ - `git_push.sh` -pushes specified folder to GitHub
+ - Initializes Git
+ - Links to remote
+ - Handles unrelated histories and merdes
+ - Pushes to `main`
+
+
+ ## Files
+ - `create_users.sh`: automates user creation and SSH setup
+ - `git_push/sh`: initializes and pushes a directory to GitHub
+ - `create_users.csv`:  stores login/password
+ - `*.txt`:  password file
+ - `create_users.log`: logs creation results
--- create_users.sh
+++ create_users.sh
@@ -2,12 +2,20 @@
  #!/bin/bash
 
 
- users=("frank" "chester" "jery")
+ log_file="create_users.log"
+ csv_file="create_users.csv"
+
+
+ echo "=== User creation started: $(date) ===" >> "log_file"
+ echo "username,password" > "$csv_file"
+
+
+ users=("frank" "chester" "jery" "rick")
 
 
  for user in "${users[@]}"; do
      if [ -d "/home/$user" ]; then
-        echo "User $user already exists"
+        echo "User $user already exists" | tee -a "$log_file"
         continue
      fi
 
@@ -18,15 +26,18 @@
      password=$(openssl rand -base64 12)
      sudo bash -c "echo 'user:$password' | chpasswd"
      echo "$password" > "${user}_pqssword.txt"
+     echo "$user,$password" >> "csv_file"
  
 
      sudo -u "$user" mkdir -p /home/$user/.ssh
      sudo -u "$user" ssh-keygen -t rsa -b 2048 -N "" -f /home/$user/.ssh/id_rsa
 
 
-     echo "User $user created. Password saved to ${user}_password.txt"
+     echo "User $user created. Password saved to ${user}_password.txt" | tee -a "$log_file"
 
 
  done
 
- echo "done"
+ echo "=== Finished: $(date) ===" >> "$log_file"
+
+ echo "Done"
